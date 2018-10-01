03fae5537cf43832ed8b80596ac47b08381ce024
Unni <unni@soupedup.com>
2018-10-01 12:39:14 +0530

    Added bootstrap, fontawesome and basic navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Boostrap module  - to install: ng add @ng-bootstrap/schematics
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
